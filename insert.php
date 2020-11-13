cb1f4b032a8b83b0e011fa38d110c09ce1ade665
Pimchanok119 <73678432+Pimchanok119@users.noreply.github.com>
2020-11-13 10:49:49 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -4,7 +4,7 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, 'pimpimchanok119.mysql.database.azure.com', 'Pimchanok119@pimpimchanok119', 'PPNuro26', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+    die('Failed to connect to MySQL PLEASE TRY AGAIN: '.mysqli_connect_error());
 }
 
 
@@ -17,7 +17,7 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    echo "New record created successfully! CONGRATULATION!";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
