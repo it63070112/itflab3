42f5dd06a0ac7b31d03a7de5d35f71958c1119af
Pimchanok119 <73678432+Pimchanok119@users.noreply.github.com>
2020-11-14 16:55:07 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,40 +1,55 @@
 <html>
 <head>
-<title>˗ ˏˋITF LABˎˊ˗</title>
+<title>ITF Lab</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  p.ex1 {
+    margin-top: 25px;
+  }
+  </style>
 </head>
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'pimpimchanok119.mysql.database.azure.com', 'Pimchanok119@pimpimchanok119', 'PPNuro26', 'ITFLab', 3306);
+mysqli_real_connect($conn, 'thanapon-226.mysql.database.azure.com', 'Jame@thanapon-226', 'thanapon-19245', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
-    die('Failed to connect to MySQL PLEASE TRY AGAIN: '.mysqli_connect_error());
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table-dark width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Action </div></th>
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <button type="button" class="btn btn-warning">EDIT</button><button type="button" class="btn btn-danger">DELETE</button>
-   </tr>
-<?php
-}
-?>
-</table>
-<center><a href="form_insert.html"><input type="submit" value="Insert" class="btn btn-info"></a></center>
-<?php
-mysqli_close($conn);
-?>
+
+<div class="container">
+    <div class="form">
+        <p class="ex1"><h1><center>ITF Show</center></h1></p>
+    </div>
+  <table width="600" border="1" class="table table-hover" class="center" class="ex1">
+    <tr class="info">
+      <th width="100"> <div align="center">Name</div></th>
+      <th width="350"> <div align="center">Comment </div></th>
+      <th width="150"> <div align="center">Action</div></th>
+    </tr>
+  <?php
+  while($Result = mysqli_fetch_array($res))
+  {
+  ?>
+    <tr>
+      <td><center><?php echo $Result['name'];?></center></td>
+      <td><center><?php echo $Result['Comment'];?></center></td>
+      <td><center><a href="form_edit.html"><input type="submit" value="Edit" class="btn btn-warning"></a>&nbsp;&nbsp;<a href="form_delete.html"><input type="submit" value="Delete"  class="btn btn-danger"></a></center></td>
+    </tr>
+  <?php
+  }
+  ?>
+  </table>
+  <center><a href="form_insert.html"><input type="submit" value="Insert" class="btn btn-info"></a></center>
+  <?php
+  mysqli_close($conn);
+  ?>
 </div>
 </body>
 </html>
